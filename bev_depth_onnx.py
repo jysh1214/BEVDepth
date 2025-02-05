ee00ea096c68bb1a8c06454fc02e9a1671d77ec5
jysh1214 <jyxemperor@gmail.com>
2025-02-06 00:26:34 +0800

    Create script to export onnx model

---
+++ bev_depth_onnx.py
@@ -0,0 +1,79 @@
+# Copyright (c) Megvii Inc. All rights reserved.
+"""
+mAP: 0.3304
+mATE: 0.7021
+mASE: 0.2795
+mAOE: 0.5346
+mAVE: 0.5530
+mAAE: 0.2274
+NDS: 0.4355
+Eval time: 171.8s
+
+Per-class results:
+Object Class    AP      ATE     ASE     AOE     AVE     AAE
+car     0.499   0.540   0.165   0.211   0.650   0.233
+truck   0.278   0.719   0.218   0.265   0.547   0.215
+bus     0.386   0.661   0.211   0.171   1.132   0.274
+trailer 0.168   1.034   0.235   0.548   0.408   0.168
+construction_vehicle    0.075   1.124   0.510   1.177   0.111   0.385
+pedestrian      0.284   0.757   0.298   0.966   0.578   0.301
+motorcycle      0.335   0.624   0.263   0.621   0.734   0.237
+bicycle 0.305   0.554   0.264   0.653   0.263   0.006
+traffic_cone    0.462   0.516   0.355   nan     nan     nan
+barrier 0.512   0.491   0.275   0.200   nan     nan
+"""
+from bevdepth.exps.base_cli import run_cli
+from bevdepth.exps.nuscenes.base_exp import \
+    BEVDepthLightningModel as BaseBEVDepthLightningModel
+from bevdepth.models.base_bev_depth import BaseBEVDepth
+
+import torch
+from onnxruntime.tools import pytorch_export_contrib_ops
+
+class BEVDepthLightningModel(BaseBEVDepthLightningModel):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.key_idxes = [-1]
+        self.head_conf['bev_backbone_conf']['in_channels'] = 80 * (
+            len(self.key_idxes) + 1)
+        self.head_conf['bev_neck_conf']['in_channels'] = [
+            80 * (len(self.key_idxes) + 1), 160, 320, 640
+        ]
+        self.head_conf['train_cfg']['code_weights'] = [
+            1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0
+        ]
+        self.model = BaseBEVDepth(self.backbone_conf,
+                                  self.head_conf,
+                                  is_train_depth=True)
+
+        # ONNX
+        x = torch.randn(1, 2, 6, 3, 256, 704, dtype=torch.float32)
+        sensor2ego_mats = torch.randn(1, 2, 6, 4, 4, dtype=torch.float32)
+        intrin_mats = torch.randn(1, 2, 6, 4, 4, dtype=torch.float32)
+        ida_mats = torch.randn(1, 2, 6, 4, 4, dtype=torch.float32)
+        sensor2sensor_mats = torch.randn(1, 2, 6, 4, 4, dtype=torch.float32)
+        bda_mat = torch.randn(1, 4, 4, dtype=torch.float32)
+
+        bevdepth = BaseBEVDepth(
+            self.backbone_conf,
+            self.head_conf,
+            is_train_depth=True
+        )
+
+        pytorch_export_contrib_ops.register()
+        torch.onnx.export(
+            bevdepth,
+            (x, sensor2ego_mats, intrin_mats, ida_mats, sensor2sensor_mats, bda_mat),
+            "bevdepth.onnx",
+            verbose=True,
+            opset_version=11,
+        )
+
+
+"""
+python3 bev_depth_onnx.py --ckpt_path bev_depth_lss_r50_256x704_128x128_24e_2key.pth -e -b 1
+"""
+if __name__ == '__main__':
+    run_cli(BEVDepthLightningModel,
+            'bev_depth_lss_r50_256x704_128x128_24e_2key')
